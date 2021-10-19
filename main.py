a39c662eddb7c7e0e5c8f1d9129c3ea412f5449a
Abemelech Mesfin <abemelech.mesfin@gmail.com>
2021-10-18 23:33:23 -0500

    Wrote tutorial for Github

--- main.py
+++ main.py
@@ -12,3 +12,20 @@ while i < 10:
     i = i + 1 
 
 #Good Job guys this is tutorial
+"""
+Going to write the instruction for how to run GitHub if you happen to forget it
+
+1. Before you start coding, make sure you have created a branch or you are in the proper branch
+    i. To create a branch the click the bottom left where it probably says "main" or if you created a branch says "YOUR BRANCH NAME" and
+    then you can choose or create a branch
+
+2. After you are on your branch, and started coding save file
+
+3. If you created any difference, it will show "M" on your file.
+
+4. Head to the 3 tab on the left side (with a blue mark on it)
+
+5. Click the plus sign and write your comment and click "Ctrl + Enter" (Win) or "Command + Enter" (Mac)
+
+6. Then click the 3 dots (...) and press Push 
+"""
